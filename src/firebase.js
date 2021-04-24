891a7c5261e19427bce7b26fd1a4cff5a94ec35b
Adriengibaud <adriengibaud@gmail.com>
2021-04-24 11:17:47 +0200

    set env variable

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,20 @@
 import firebase from 'firebase';
 
+console.log(process.env.REACT_APP_FIREBASE_KEY);
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyBUXAI8xFyE0EMk1yJNbyl3GWSFky3uq3o',
-  authDomain: 'invoices-managerio.firebaseapp.com',
-  projectId: 'invoices-managerio',
-  storageBucket: 'invoices-managerio.appspot.com',
-  messagingSenderId: '58819077627',
-  appId: '1:58819077627:web:21f35886d87214e267575e',
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // eslint-disable-next-line no-unused-vars
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
+const auth = firebaseApp.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 
 export { auth, provider };
