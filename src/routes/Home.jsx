d5f7ce52ca06f74060630fc5c23cc9b60f695c49
Adriengibaud <adriengibaud@gmail.com>
2021-04-24 11:48:36 +0200

    test

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export const Home = () => (
   <>
-    <Link to="/invoice">View invoices</Link>
+    <Link to="/invoice">View invoices .</Link>
   </>
 );
 
