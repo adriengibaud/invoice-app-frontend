77ea9f267fae8f952ac9360f57fd34762e297198
Adriengibaud <adriengibaud@gmail.com>
2021-04-24 11:32:14 +0200

    env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,5 @@
 import firebase from 'firebase';
 
-console.log(process.env.REACT_APP_FIREBASE_KEY);
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
