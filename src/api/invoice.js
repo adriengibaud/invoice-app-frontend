acb11fe10554e04ec061e3682e0729a92bf447df
Adriengibaud <adriengibaud@gmail.com>
2021-05-18 10:54:35 +0200

    changed adress

--- src/api/invoice.js
+++ src/api/invoice.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/invoice';
+const baseUrl = 'http://localhost:3001/api/invoice';
 
 const getAll = (userId) => {
   const request = axios.get(`${baseUrl}/${userId}`);
